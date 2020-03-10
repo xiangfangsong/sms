96a5618ac57ec0d11793ca4a123b153e749db8c7
xiangfangsong <cnxfs@qq.com>
2020-03-10 19:13:57 +0800

    upload

---
+++ src/main/java/com/example/test/controller/SmsController.java
@@ -0,0 +1,38 @@
+package com.example.test.controller;
+
+import com.example.test.entity.Sms;
+import com.github.qcloudsms.SmsSingleSender;
+import com.github.qcloudsms.SmsSingleSenderResult;
+import com.github.qcloudsms.httpclient.HTTPException;
+import org.json.JSONException;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping(value = "/sms")
+public class SmsController {
+    @RequestMapping(value = "/sendCode", method = RequestMethod.POST)
+    public void sms(@RequestBody Sms sms){
+        int appid = 1000000003;
+        String appkey = "e*****************************71";
+        int templateId = 123456; //模板ID
+        String smsSign = "你的签名";
+        try{
+            String[] params = {sms.getCode(), Integer.toString(sms.getMin())}; //短信中的参数
+            SmsSingleSender ssender = new SmsSingleSender(appid,appkey);
+            SmsSingleSenderResult result = ssender.sendWithParam("86",sms.getPhoneNumber(),templateId,
+                    params,smsSign,"","");
+            System.out.println(result);
+        }catch (HTTPException e){
+            e.printStackTrace();
+        }catch (JSONException e){
+            e.printStackTrace();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/com/example/test/entity/Sms.java
@@ -0,0 +1,31 @@
+package com.example.test.entity;
+
+public class Sms {
+    String phoneNumber;
+    String code;
+    int min;
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public int getMin() {
+        return min;
+    }
+
+    public void setMin(int min) {
+        this.min = min;
+    }
+}
